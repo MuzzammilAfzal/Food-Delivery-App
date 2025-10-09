b2cd93f35ecd122b6b212d0e9a440a059b2d3a0f
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-10-09 20:01:05 +0530

    dot env config

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors=require('cors')
 const userRouter=require("./router/user")
 const app = express()
 const port = 3000
+require('dotenv').config();
 
 app.use(cors({origin:"*",
     methods:["GET","POST","PUT","DELETE"],credentials:true
@@ -18,4 +19,4 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-mongoose.connect("mongodb+srv://muzzu2605afzall:9972228752.@clusterafzal.mzc6v.mongodb.net/",{dbName:"foodDeliveryApp"})
+mongoose.connect(process.env.MONGODB_URL,{dbName:"foodDeliveryApp"})
