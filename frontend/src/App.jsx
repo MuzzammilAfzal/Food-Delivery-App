6a89b087688a95dd86daa379afd47ea748c055cd
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-10-15 18:49:18 +0530

    bug1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/home';
+import Home from './pages/Home';
 import Layout from './components/Layout/Layout';
 import Restaurant from './pages/Restaurant';
 import ConfirmOrder from './pages/ConfirmOrder';
